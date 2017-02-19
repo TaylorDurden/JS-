2addcbde32d8b906481873517a5cbd5572813baa
Taylor <258266864@qq.com>
2017-02-19 23:01:25 +0800

    next to 按钮和第一帧层级关系设置

--- js/carousel.js
+++ js/carousel.js
@@ -1,9 +1,59 @@
 ;(function($) { /* 加; 是为了避免引入的文件没有;闭合而导致产生错误。 */
 	var Carousel = function(poster) {
-		alert(poster);
+
+		this.poster = poster;
+		this.posterContainer = poster.find("ul.poster-list");
+		this.posterFirstItem = this.posterContainer.find('li').eq(0);
+		this.nextButton = poster.find("div.poster-next-btn");
+		this.prevButton = poster.find("div.poster-prev-btn");
+		this.config = {
+			"width": 1000, // 幻灯片的宽度
+			"height": 270, // 幻灯片的高度
+			"posterWidth": 640, // 幻灯片第一帧的宽度
+			"posterHeight": 270, // 幻灯片第一帧的高度
+			"scale": 0.9, 
+			"speed": 500,
+			"verticalAlignment": "middle"
+		};
+
+		$.extend(this.config, this.getConfig());
+		this.setConfigToElements();
 	};
 
-	Carousel.prototype = {};
+	Carousel.prototype = {
+		//设置配置参数值来控制基本的宽高比例等
+		setConfigToElements: function() {
+			this.poster.css({
+				width: this.config.width,
+				height: this.config.height
+			});
+
+			this.posterContainer.css({
+				width: this.config.width,
+				height: this.config.height
+			});
+
+			//计算上下切换按钮的宽度
+			var computedWidthForSwitchButton = (this.config.width - this.config.posterWidth) / 2;
+			this.nextButton.css({
+				width: computedWidthForSwitchButton,
+				height: this.config.height
+			});
+			this.prevButton.css({
+				width: computedWidthForSwitchButton,
+				height: this.config.height
+			});
+
+			this.posterFirstItem.css({
+				left: computedWidthForSwitchButton
+			});
+		},
+		// 获取人工配置参数
+		getConfig: function() {
+			var config = this.poster.data('config');
+			return config !==""&& config != null ? config : {};			
+		}
+	};
 
 	Carousel.init = function(posters) {
 		var _this_ = this; /* this -> Carousel */
