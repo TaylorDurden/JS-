a5e9cffcd4c8d860c20bf79bad3f3d5514e7af63
Taylor <258266864@qq.com>
2017-02-20 23:36:59 +0800

    next to 右边帧的位置关系设置

--- js/carousel.js
+++ js/carousel.js
@@ -4,8 +4,11 @@
 		this.poster = poster;
 		this.posterContainer = poster.find("ul.poster-list");
 		this.posterFirstItem = this.posterContainer.find('li').eq(0);
+		this.posterItems = poster.find('li.poster-item');
+
 		this.nextButton = poster.find("div.poster-next-btn");
 		this.prevButton = poster.find("div.poster-prev-btn");
+
 		this.config = {
 			"width": 1000, // 幻灯片的宽度
 			"height": 270, // 幻灯片的高度
@@ -21,6 +24,33 @@
 	};
 
 	Carousel.prototype = {
+		/*设置剩余的帧的位置关系*/
+		setPosterPosition: function() {
+			var sliceItems = this.posterItems.slice(1),
+				sliceSize = sliceItems.size()/2,
+				rightSlice = sliceItems.slice(0, sliceSize),
+				indexLevel = Math.floor(this.posterItems.size()/2);
+
+			// 设置右边帧的位置关系和宽度高度top
+			var rightWidth = this.config.posterWidth,
+				rightHeight = this.config.posterHeight,
+				gap = ((this.config.width - this.config.posterWidth) / 2) / indexLevel;
+
+
+			rightSlice.each(function(i) {
+				indexLevel--;
+				rightWidth = rightWidth * self.config.scale;
+				rightHeight = rightHeight * self.config.scale;
+				$(this).css({
+					zIndex: xx,
+					width: ww,
+					height: hh,
+					opcatiy: oo,
+					left: ll
+				});
+			});
+		},
+
 		//设置配置参数值来控制基本的宽高比例等
 		setConfigToElements: function() {
 			this.poster.css({
@@ -37,15 +67,20 @@
 			var computedWidthForSwitchButton = (this.config.width - this.config.posterWidth) / 2;
 			this.nextButton.css({
 				width: computedWidthForSwitchButton,
-				height: this.config.height
+				height: this.config.height,
+				zIndex: Math.ceil(this.posterItems.size() / 2) // 向上取整
 			});
 			this.prevButton.css({
 				width: computedWidthForSwitchButton,
-				height: this.config.height
+				height: this.config.height,
+				zIndex: Math.ceil(this.posterItems.size() / 2) 
 			});
 
 			this.posterFirstItem.css({
-				left: computedWidthForSwitchButton
+				left: computedWidthForSwitchButton,
+				width: this.config.width,
+				height: this.config.height,
+				zIndex: Math.floor(this.posterItems.size() / 2) // 向下取整
 			});
 		},
 		// 获取人工配置参数
